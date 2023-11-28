e548baaef75e9686ff31fe64c7fbeb3001d722cd
Adarsh Raj <77489636+Adarsh-Raj04@users.noreply.github.com>
2023-11-28 07:17:53 +0530

    Update EmailSection.jsx

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import { useForm, ValidationError } from "@formspree/react";
 
 const EmailSection = () => {
-  const [state, handleSubmit] = useForm(process.env.key);
+  const [state, handleSubmit] = useForm(process.env.FORMSPREE_KEY);
   var emailSubmitted;
   if (state.succeeded) {
     emailSubmitted = true;
